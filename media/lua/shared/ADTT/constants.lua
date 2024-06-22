30a08c87a4de0afb4c8a619ecd22bbde6122ded4
aoqia194 <30682549+aoqia194@users.noreply.github.com>
2024-06-22 16:00:50 +1000

    feat(update): Get up to speed!

--- media/lua/shared/ADTT/constants.lua
+++ media/lua/shared/ADTT/constants.lua
@@ -9,7 +9,7 @@ local constants = {}
 constants.IS_DEBUG = getDebug() == true
 constants.IS_LAST_STAND = getCore():getGameMode() == "LastStand"
 
-constants.MOD_ID = "AoqiaDynamicTranslations"
+constants.MOD_ID = "AoqiaDynamicTranslationsAPI"
 constants.MOD_VERSION = "0.0.1"
 
 return constants
