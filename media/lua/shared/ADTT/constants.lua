1dbeaab0a2f433cd6be6f4af6efda9a867b51c2d
aoqia194 <30682549+aoqia194@users.noreply.github.com>
2024-06-22 15:50:24 +1000

    feat(chore): Update mod ID

--- media/lua/shared/ADTT/constants.lua
+++ media/lua/shared/ADTT/constants.lua
@@ -9,7 +9,7 @@ local constants = {}
 constants.IS_DEBUG = getDebug() == true
 constants.IS_LAST_STAND = getCore():getGameMode() == "LastStand"
 
-constants.MOD_ID = "AoqiaDynamicTranslationsTest"
+constants.MOD_ID = "AoqiaDynamicTranslations"
 constants.MOD_VERSION = "0.0.1"
 
 return constants
