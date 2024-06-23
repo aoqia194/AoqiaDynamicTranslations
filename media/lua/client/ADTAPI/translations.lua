118f2145e289ed6964f55068db6ed463c08476c7
aoqia194 <30682549+aoqia194@users.noreply.github.com>
2024-06-23 13:53:44 +1000

    feat(lang): Add fake language reloading
    Isn't quite what I'd like and I would add real live language reloading if I could but I can't so an illusion is the best I can do for now.

--- media/lua/client/ADTAPI/translations.lua
+++ media/lua/client/ADTAPI/translations.lua
@@ -2,6 +2,8 @@
 --       Manages a translation cache that is used to change translations      --
 -- -------------------------------------------------------------------------- --
 
+local logger = require("ADTAPI/logger")
+
 -- ------------------------------ Module Start ------------------------------ --
 
 local translations = {}
@@ -11,12 +13,90 @@ translations.original_cache = {}
 -- Stores the most up-to-date translations for the game in key-value pairs.
 translations.cache = {}
 
+--- A code following the IETF language code standard.
+--- REF: https://en.wikipedia.org/wiki/IETF_language_tag
+--- @alias language_code string
+
+--- Language codes
+--- @class language_codes
+--- @field CHINESE language_code
+--- @field CHINESE_TAIWAN language_code
+--- @field DUTCH language_code
+--- @field ENGLISH language_code
+--- @field ENGLISH_BRITAIN language_code
+--- @field FINNISH language_code
+--- @field FRENCH language_code
+--- @field GERMAN language_code
+--- @field ITALIAN language_code
+--- @field JAPANESE language_code
+--- @field KOREAN language_code
+--- @field POLISH language_code
+--- @field PORTUGUESE language_code
+--- @field PORTUGUESE_BRAZIL language_code
+--- @field SPANISH language_code
+--- @field SPANISH_MEXICO language_code
+--- @field SWEDISH language_code
+translations.language_codes = {
+    CHINESE = "ZH-CN",
+    CHINESE_TAIWAN = "ZH-TW",
+    DUTCH = "NL",
+    ENGLISH = "EN",
+    ENGLISH_BRITAIN = "EN-GB",
+    FINNISH = "FI",
+    FRENCH = "FR",
+    GERMAN = "DE",
+    ITALIAN = "IT",
+    JAPANESE = "JA",
+    KOREAN = "KO",
+    POLISH = "PL",
+    PORTUGUESE = "PT",
+    PORTUGUESE_BRAZIL = "PT-BR",
+    SPANISH = "ES",
+    SPANISH_MEXICO = "ES-MX",
+    SWEDISH = "SV",
+}
+
+--- Changes a translation's language in-place to the specified language.
+--- @param key string
+--- @param language_code language_code
+function translations.update_language(key, language_code)
+    local new_key = (key .. "__ADTAPI_" .. language_code)
+
+    -- TODO: Refactor this cancer
+
+    -- Try to get translation using cache
+    local translation = translations.find(new_key)
+    if translation == nil then
+        -- Try to get translation using getText
+        translation = getText(new_key)
+        if translation == "nil" then
+            logger.debug("Couldn't get new language translation, falling back to default...")
+
+            -- Try to get DEFAULT using cache
+            translation = translations.find_original(key)
+            if translation == nil then
+                logger.warn("Translation (%s) was nil when trying to update language to (%s).", key, language_code)
+                return
+            end
+        end
+    end
+
+    translations.update(key, translation)
+end
+
+--- Resets a translation in the cache to it's first ever cached state. If it doesn't exist, nothing happens.
+--- @param key string
+function translations.reset(key)
+    local original = translations.find_original(key)
+    if original == nil then return end
+
+    translations.update(key, original)
+end
+
 --- Similar to find_original but returns if it is found instead of the value.
 --- @param key string
 --- @return boolean does_exist
 function translations.exists_original(key)
-    key = tostring(key)
-
     return translations.original_cache[key] ~= nil
 end
 
@@ -24,8 +104,6 @@ end
 --- @param key string
 --- @return string|nil translation
 function translations.find_original(key)
-    key = tostring(key)
-
     return translations.original_cache[key] or nil
 end
 
@@ -33,9 +111,6 @@ end
 --- @param key string
 --- @param value string
 function translations.update_original(key, value)
-    key = tostring(key)
-    value = tostring(value)
-
     if not translations.exists_original(key) then return end
     translations.original_cache[key] = value
 end
@@ -45,31 +120,14 @@ end
 --- @param key string
 --- @param value string
 function translations.create_original(key, value)
-    -- For the dummies...
-    key = tostring(key)
-    value = tostring(value)
-
     if translations.exists_original(key) then return end
     translations.original_cache[key] = value
-end
-
---- Resets a translation in the cache to it's first ever cached state. If it doesn't exist, nothing happens.
---- @param key string
-function translations.reset(key)
-    key = tostring(key)
-
-    local original = translations.find_original(key)
-    if original == nil then return end
-
-    translations.update(key, original)
 end
 
 --- Similar to find but returns if it is found instead of the value.
 --- @param key string
 --- @return boolean does_exist
 function translations.exists(key)
-    key = tostring(key)
-
     return translations.cache[key] ~= nil
 end
 
@@ -77,8 +135,6 @@ end
 --- @param key string
 --- @return string|nil translation
 function translations.find(key)
-    key = tostring(key)
-
     return translations.cache[key] or nil
 end
 
@@ -86,9 +142,6 @@ end
 --- @param key string
 --- @param value string
 function translations.update(key, value)
-    key = tostring(key)
-    value = tostring(value)
-
     if not translations.exists(key) then return end
     translations.cache[key] = value
 end
@@ -97,10 +150,6 @@ end
 --- @param key string
 --- @param value string
 function translations.create(key, value)
-    -- For the dummies...
-    key = tostring(key)
-    value = tostring(value)
-
     translations.create_original(key, value)
 
     if translations.exists(key) then return end
