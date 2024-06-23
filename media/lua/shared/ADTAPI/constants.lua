99d14e0ae8218e1b9eb9094fdbe9aa7f2098ce15
aoqia194 <30682549+aoqia194@users.noreply.github.com>
2024-06-23 16:55:46 +1000

    chore(version): Increment version

--- media/lua/shared/ADTAPI/constants.lua
+++ media/lua/shared/ADTAPI/constants.lua
@@ -10,6 +10,6 @@ constants.IS_DEBUG = getDebug() == true
 constants.IS_LAST_STAND = getCore():getGameMode() == "LastStand"
 
 constants.MOD_ID = "AoqiaDynamicTranslationsAPI"
-constants.MOD_VERSION = "0.0.1"
+constants.MOD_VERSION = "1.0.0"
 
 return constants
