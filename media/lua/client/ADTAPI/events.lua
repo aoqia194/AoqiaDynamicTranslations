ef404676da8bc3a12a1044c1916db2f589f995d4
aoqia194 <30682549+aoqia194@users.noreply.github.com>
2024-06-23 13:54:48 +1000

    fix(chore): Remove test event.

--- media/lua/client/ADTAPI/events.lua
+++ media/lua/client/ADTAPI/events.lua
@@ -17,24 +17,6 @@ local logger       = require("ADTAPI/logger")
 
 local events       = {}
 
---- @param key_code integer
-function events.on_key_start_pressed(key_code)
-    local key = "IGUI_health_Overall_Body_Status"
-
-    if key_code == 157 then -- RCTRL
-        logger.debug("Pressed Key RCTRL")
-        local text = translations.find(key)
-        if text == nil then return end
-
-        local new_text = text:sub(-1) .. text:sub(1, -2)
-        translations.update(key, new_text)
-    elseif key_code == 54 then -- RSHIFT
-        logger.debug("Pressed key RSHIFT")
-        if not translations.exists(key) and translations.exists_original(key) then return end
-        translations.reset(key)
-    end
-end
-
 function events.on_game_boot()
     logger.debug("Hooking getText...")
     hooks.o_get_text = getText
@@ -44,7 +26,6 @@ end
 function events.register()
     logger.debug("Registering events...")
 
-    -- Events.OnKeyStartPressed.Add(events.on_key_start_pressed)
     Events.OnGameBoot.Add(events.on_game_boot)
 end
 
