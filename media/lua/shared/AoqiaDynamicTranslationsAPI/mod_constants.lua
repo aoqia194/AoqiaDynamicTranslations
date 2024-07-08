9600c888ada1d1a19c85ae2756e1d62f4f887965
aoqia194 <30682549+aoqia194@users.noreply.github.com>
2024-07-08 13:20:10 +1000

    feat: Use new logger instance

--- media/lua/shared/AoqiaDynamicTranslationsAPI/mod_constants.lua
+++ media/lua/shared/AoqiaDynamicTranslationsAPI/mod_constants.lua
@@ -2,11 +2,15 @@
 --                   Stores constants to be used everywhere.                  --
 -- -------------------------------------------------------------------------- --
 
+local logger = require("AoqiaZomboidUtils/logger")
+
 -- ------------------------------ Module Start ------------------------------ --
 
 local mod_constants = {}
 
 mod_constants.MOD_ID = "AoqiaDynamicTranslationsAPI"
-mod_constants.MOD_VERSION = "1.0.2"
+mod_constants.MOD_VERSION = "1.0.3"
+
+mod_constants.LOGGER = logger:new(mod_constants.MOD_ID)
 
 return mod_constants
